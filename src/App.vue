6322882020f72f99dc22f4fc360a27b4b8f2f676
Lukasz <lukaszmodelski90@gmail.com>
2019-02-22 17:13:54 +0100

    fetched posts and pages form REST API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h2 v-if="this.dataLoaded">Analiza ryzyka</h2>
+    <div v-if="dataLoaded" v-html="posts['analiza-ryzyka'].content.rendered" class="content">
+
+    </div>
+    <!-- <div v-if="dataLoaded" class="content">
+      {{ posts['analiza-ryzyka'].content.rendered }}
+    </div> -->
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+        posts: Object,
+        pages: Object,
+        dataLoaded: false
+    }
+  },
+  methods: {
+    loadPosts(){
+      axios.get('http://www.modepharm.pl/wp-json/wp/v2/posts/?per_page=100')
+      .then((response) => {
+          // reverse data because of adding this to object in next step
+          response.data = response.data.reverse();
+          this.posts = [];
+          for(let i=0;i<response.data.length;i++){
+              let obj = {
+                  [response.data[i].slug]: response.data[i]
+              };
+              this.posts = Object.assign(obj, this.posts);
+          }
+          this.dataLoaded = true;
+          // do in next thick, vue must see !isLoading is true
+          // this.$nextTick(()=>{
+          //     eventBus.$emit('data-loaded',true);
+          // })
+          // console.log('Data loaded!');
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+    },
+    loadPages(){
+      axios.get('http://www.modepharm.pl/wp-json/wp/v2/pages/?per_page=100')
+      .then((response) => {
+          // reverse data because of adding this to object in next step
+          response.data = response.data.reverse();
+          this.pages = [];
+          for(let i=0;i<response.data.length;i++){
+              let obj = {
+                  [response.data[i].slug]: response.data[i]
+              };
+              this.pages = Object.assign(obj, this.pages);
+          }
+          // this.dataLoaded = true;
+          // do in next thick, vue must see !isLoading is true
+          // this.$nextTick(()=>{
+          //     eventBus.$emit('data-loaded',true);
+          // })
+          // console.log('Data loaded!');
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+    }
+  },
+  created(){
+    this.loadPosts();
+    this.loadPages();
   }
 }
 </script>
